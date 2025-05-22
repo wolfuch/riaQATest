f01f192ddfda6bdb45947b39e6c7090cbb785f42
wolfuch <n.arellano.t@gmail.com>
2025-05-21 21:21:20 -0400

    update Ria Test

---
+++ src/main/java/riaTest/getStarted.java
@@ -0,0 +1,38 @@
+package riaTest;
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+import java.util.regex.Pattern;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+
+public class getStarted {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+
+            //Navigate and verify the buttons
+            page.navigate("https://secure.riamoneytransfer.com/");
+            page.waitForLoadState();
+            //page.pause();
+            //accept the coockies
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Allow all cookies")).click();
+
+            assertThat(page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^Phone or email$")))).isEnabled();
+            assertThat(page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Password"))).isEnabled();
+            assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Register"))).isEnabled();
+
+            //When Register button is clicked, the user is redirected to country selection page.
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Register")).click();
+            assertThat(page).hasURL("https://secure.riamoneytransfer.com/registration");
+
+
+            page.waitForTimeout(1500);
+
+
+
+        }
+    }
+}
--- src/main/java/riaTest/riaCalculator.java
+++ src/main/java/riaTest/riaCalculator.java
@@ -1,11 +1,11 @@
-package org.example;
+package riaTest;
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.AriaRole;
 import java.util.regex.Pattern;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 
-public class Main {
+public class riaCalculator {
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
 
